e3299aea633f37d5a0d67abb9dbe00e26bc4991b
junyng <pom0894@naver.com>
2021-12-12 16:57:18 +0900

    Add project

---
+++ Cherry/CherryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CherryApp.swift
+//  Cherry
+//
+//  Created by junyeong-cho on 2021/12/12.
+//
+
+import SwiftUI
+
+@main
+struct CherryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
